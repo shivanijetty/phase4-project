51fa59f4ff3e1336550d6dc59c19b062133171c9
Helen <helenmperham@gmail.com>
2023-01-26 16:23:52 -0500

    added upload widget

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Login from './Login'
 import Profile from './Profile'
+import UploadWidget from './UploadWidget'
 // import User from './User'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Profile />
+      <UploadWidget/>
       <Login />      
     </div>
   )
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux'
 
 
+
 function Profile() {
     const user = useSelector((state) => state.user.value)
   return(
