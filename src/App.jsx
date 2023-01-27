99981c82fe9e03a4706afbbc468527b693dc91eb
Helen <helenmperham@gmail.com>
2023-01-26 19:22:52 -0500

    added UploadWidget and GalleryWidget components

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import './App.css'
 import Login from './Login'
 import Profile from './Profile'
 import UploadWidget from './UploadWidget'
+import GalleryWidget from './GalleryWidget'
 // import User from './User'
 
 function App() {
   
   return (
     <div className="App">
-      <Profile />
+      {/* <Profile /> */}
       <UploadWidget/>
+      {/* <GalleryWidget/> */}
       <Login />      
     </div>
   )
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
+// import { GalleryWidget } from './GalleryWidget'
+// import { UploadWidget } from './UploadWidget'
+
 
 
 
@@ -7,6 +10,8 @@ function Profile() {
     const user = useSelector((state) => state.user.value)
   return(
     <div>
+      {/* <GalleryWidget/> */}
+      {/* <UploadWidget/> */}
       <h1>Profile Page</h1>
       <p>Name: {user.name} </p>
       <p>Age: {user.age} </p>
